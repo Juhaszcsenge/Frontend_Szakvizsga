9fec72df2a66810fe61e6dc4a74876cfa0f3e508
Kerecsényi Márk <karatelovag@gmail.com>
2023-03-14 15:22:38 +0100

    authentication Singup form

--- src/Pages/Singup.tsx
+++ src/Pages/Singup.tsx
@@ -1,16 +1,82 @@
 import React, {Component} from "react";
 import { Container } from 'react-bootstrap';
 import {Link} from "react-router-dom";
+import {ResponseMess} from 
 
 
 interface State{
+  message: string[];
   singEmail: string;
+  singFullname: string;
   singPass: string;
+  singRePass: string;
 }
 
 
+export default  class Singup extends Component <{}, State>{
+  constructor(props: {}){
+    super(props);
 
-export default  class Singup extends Component {
+    this.state = {
+      message: [],
+      singEmail: '',
+      singFullname: '',
+      singPass: '',
+      singRePass: ''
+    }
+  }
+    handleRegister = async () => {
+       if (this.state.singFullname.trim() === ''){
+        this.setState({message: ['Kérjük adja meg a teljes nevét!'] })
+        return;
+       }
+       else if(this.state.singEmail === ''){
+        this.setState({message: ['Kérjük adja meg az email címét']})
+        return;
+       }
+       else if(this.state.singPass === ''){
+        this.setState({message: ['Kötelező jelszót megadni']})
+        return;
+       }
+       else if(this.state.singRePass === ''){
+        this.setState({message: ['A két jelszó  nem egyezik']})
+        return;
+       }
+       else{
+        const date ={
+          fullName: this.state.singFullname,
+          email: this.state.singEmail,
+          password: this.state.singPass,
+          repassword: this.state.singRePass
+        };
+
+        let response = await fetch("http://localhost:3000/user/register",{
+          method: 'POST',
+          /*headers: {
+          'Authorization' : 'Bearer ' + a0565e94fb8fbe8a4343c23da6616414f35add2139d11da8fcea65943484ce7e
+          'Content-Type': 'application/json'
+          },*/
+          body: JSON.stringify(Register request),
+        });
+
+        if(response.ok){
+          this.setState({
+            singFullname: '',
+            singEmail: '',
+            singPass: '',
+            singRePass: ''
+          })
+          this.setState({message: ['Sikeres regisztráció']})
+        }
+        else{
+          const sing = await response.json() as ResponseMess 
+          this.setState({message: sing.message})
+        }
+
+       }
+    }
+  }
+
     render() {
         return(
           <Container className="regcon">
@@ -18,16 +84,8 @@ export default  class Singup extends Component {
             <div className="form-box"></div>
             <h3 style={{margin:'top'}}>Sign Up</h3>
             <div className="input-box">
-              <label>Vezeték név</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="First name"
-              />
-            </div>
-            <div className="input-box">
-              <label>Kereszt név</label>
-              <input type="text" className="form-control" placeholder="Second name" />
+              <label>Teljes név</label>
+              <input type="text" value={this.state.singFullName} onChange={e => this.setState({singFullName: e.currentTarget.value})} className="form-control" placeholder="Full name" />
             </div>
             <div className="input-box">
               <label>Email cím</label>
@@ -46,7 +104,7 @@ export default  class Singup extends Component {
               />
             </div>
             <div className="d-grid">
-              <button type="submit" className="btn btn-primary">
+              <button type="submit" className="btn btn-primary" onClick={this.handleRegister}>
                 Regisztráció
               </button>
             </div>
