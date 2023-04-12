4ae11cdcb9932f2cff13edbccc395eb3a528a0c0
Juhász Csenge <csenge799@gmail.com>
2023-04-13 00:13:48 +0200

    img

---
+++ src/Pages/Donwload.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+
+const Donwload = () => {
+    return <section>
+        <Container className="app_conatiner">
+            <Row>
+                <Col lg='6' md='6'>
+                    <div className="app_img">
+                        <img src=""></img>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+}
